7c6c7dd72d4575334648ecb10b7f540249ca35c9
Jimmy <chen.jimmym@gmail.com>
2017-10-18 13:20:09 -0700

    done

--- apps/gold/views.py
+++ apps/gold/views.py
@@ -1,22 +1,37 @@
 from django.shortcuts import render, redirect
 import random
+from time import gmtime, strftime
 
 # Create your views here.
 def index(request):
     if not 'gold_amount' in request.session:
         request.session['gold_amount'] = 0
+    if not 'messages' in request.session:
+            request.session['messages'] = []
     return render(request, 'gold/index.html')
 
 def process_gold(request):
     if request.method == "POST":
         if request.POST['place'] == 'farm':
-            request.session['gold_amount'] += random.randrange(10,21)
+            temp_gold = random.randrange(10,21)
+            request.session['gold_amount'] += temp_gold
         if request.POST['place'] == 'cave':
-            request.session['gold_amount'] += random.randrange(5,11)
+            temp_gold = random.randrange(5,11)
+            request.session['gold_amount'] += temp_gold
         if request.POST['place'] == 'house':
-            request.session['gold_amount'] += random.randrange(2,6)
+            temp_gold = random.randrange(2,6)
+            request.session['gold_amount'] += temp_gold
         if request.POST['place'] == 'casino':
-            request.session['gold_amount'] += random.randrange(-50,50)
+            temp_gold = random.randrange(-50,50)
+            request.session['gold_amount'] += temp_gold
         if request.POST['place'] == 'reset':
             request.session['gold_amount'] = 0
+            request.session['messages'] = []
+            return redirect('/')
+        if temp_gold > 0:
+            earn_lost = 'positive'
+        else:
+            earn_lost = 'negative'
+        temp_message = {'gold_amount':temp_gold,'place':request.POST['place'],'earn_lost':earn_lost, 'time':strftime("%Y-%m-%d %H:%M:%S", gmtime())}
+        request.session['messages'].append(temp_message)
     return redirect('/')
